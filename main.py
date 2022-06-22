e4041b45b6d4d50b215d23b202acdb495c084463
Юрій <yurijlitviniii@gmail.com>
2022-06-22 22:18:48 +0300

    class main paddle created paddle moves up and down

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from turtle import Screen
+from paddle import Paddle
 
 
 screen = Screen()
+paddle = Paddle()
 
 screen.setup(width=1200, height=600)
 screen.bgcolor("black")
+
+screen.listen()
+screen.onkey(paddle.up, "Up")
+screen.onkey(paddle.down, "Down")
+
+
+
 
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,7 +1,30 @@
 from turtle import Turtle
+STARTING_POSITIONS = [(550, -20), (550, 0), (550, 20)]
 
 
 class Paddle:
 
     def __init__(self):
-        pass
+        self.parts = []
+        self.create_paddle()
+
+    def create_paddle(self):
+        for position in STARTING_POSITIONS:
+            self.add_part(position)
+
+    def add_part(self, position):
+        new_part = Turtle("square")
+        new_part.color("white")
+        new_part.penup()
+        new_part.setheading(90)
+        new_part.goto(position)
+        self.parts.append(new_part)
+
+    def up(self):
+        for part in self.parts:
+            part.forward(10)
+
+    def down(self):
+        for part in self.parts:
+            part.backward(10)
+
