92cd24c9e2c74c274aaf1ccc61cde8c0bfc95911
Юрій <yurijlitviniii@gmail.com>
2022-06-23 19:34:57 +0300

    class Paddle: inheritance of Turtle class added initialization of super class added to the constructor

--- paddle.py
+++ paddle.py
@@ -1,23 +1,22 @@
 from turtle import Turtle
 WIDTH = 1
 HEIGHT = 5
-# X_POS = 350
-# Y_POS = 0
 
 
-class Paddle:
+class Paddle(Turtle):
 
     def __init__(self, position):
+        super().__init__()
+        self.shape("square")
         self.position = position
-        self.paddle = Turtle("square")
-        self.paddle.color("white")
-        self.paddle.penup()
-        self.paddle.turtlesize(WIDTH, HEIGHT)
-        self.paddle.setpos(position)
-        self.paddle.setheading(90)
+        self.color("white")
+        self.penup()
+        self.turtlesize(WIDTH, HEIGHT)
+        self.setpos(position)
+        self.setheading(90)
 
     def up(self):
-        self.paddle.forward(20)
+        self.forward(20)
 
     def down(self):
-        self.paddle.backward(20)
+        self.backward(20)
