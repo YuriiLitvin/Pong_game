5b33f8410fa2032c2374b793336f581c5b166573
Юрій <yurijlitviniii@gmail.com>
2022-06-23 19:01:15 +0300

    class Paddle created paddle instance attributes set

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 
-screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
+# screen.listen()
+# screen.onkey(paddle.up, "Up")
+# screen.onkey(paddle.down, "Down")
 
 
 
---
+++ paddle.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+WIDTH = 1
+HEIGHT = 5
+X_POS = 350
+Y_POS = 0
+
+
+class Paddle:
+
+    def __init__(self):
+        self.paddle = Turtle("square")
+        self.paddle.color("white")
+        self.paddle.penup()
+        self.paddle.turtlesize(WIDTH, HEIGHT)
+        self.paddle.setpos(X_POS, Y_POS)
+        self.paddle.setheading(90)
+
